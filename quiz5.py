e4c35cb5a4ae9f6234c9e52b0bf052654ce1c374
jjeongmin98 <eodjr05@naver.com>
2022-09-19 10:22:20 +0900

    v

--- quiz5.py
+++ quiz5.py
@@ -5,3 +5,13 @@
 # 조건 2 : 당신은 소요시간 5분 ~ 15분 사이의 승객만 매칭해야한다.
 # //출처 : 나도코딩
 
+from random import *
+count = 0
+for i in range(1,51):
+    n = int(randrange(5,50))
+    if n in range(5,15):
+        print("[o] {0}번째 손님 (소요시간 : {1}분".format(i,n))
+        count += 1
+    else:
+        print("[ ] {0}번째 손님 (소요시간 : {1}분".format(i,n))
+print("총 탑승 승객 : {0}분".format(count))
